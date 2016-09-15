c5ff6fb244dac0b0b49722b92d0db5c9e99d2865
diego <diegolv9999@hotmail.com>
2016-09-14 20:39:34 -0500

    inicio del proyecto appJava se crea una clase usuario  ej java y se crean los atributos usuario y clave tipo: nueva funcionalidad

---
+++ appJava/src/appjava/modelo/Usuario.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appjava.modelo;
+
+/**
+ *
+ * @author diego
+ */
+public class Usuario {
+    
+    private String usuario="";
+     private String clave="";
+
+    public Usuario() {
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+   
+     
+     
+    
+}
